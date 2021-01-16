b2bfcfe5484403fe7092b0514e9b33c31345f424
StepanGavrilov <gavrilovstepan78@gmail.com>
2021-01-16 18:08:28 +0300

    1

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ from datetime import timedelta
 
 
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = '%8@*zy^tpe*cze_@ai2cru#c#uij3xtcr98q0ww&b1v)_pixud'
 DEBUG = True
 ALLOWED_HOSTS = ['*']
 
@@ -76,11 +76,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
+        'NAME': 'rentworkplace',
+        'USER': 'postgres',
+        'PASSWORD': '1',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -140,9 +140,8 @@ SIMPLE_JWT = {
 }
 
 
-LANGUAGE_CODE = config('LANGUAGE_CODE')
-TIME_ZONE = config('TIME_ZONE')
-
+LANGUAGE_CODE = 'ru'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 USE_L10N = True
